29a9e482ee3542e6be71220475c7850582152081
artem <artemminsadyrov@gmail.com>
2026-03-15 14:00:31 +0100

    Add UserDashboard and Deposit pages; add login-to-dashboard flow in LoginPage

---
+++ src/test/java/pages/DepositPage.java
@@ -0,0 +1,67 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import io.appium.java_client.MobileBy;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class DepositPage {
+    private static final Logger log = LoggerFactory.getLogger(DepositPage.class);
+
+    private static final String ACCOUNT_ID_INPUT = "//android.widget.EditText[@text='Account ID']";
+    private static final String ACCOUNT_NUMBER_INPUT = "//android.widget.EditText[@text='Account Number']";
+    private static final String AMOUNT_INPUT = "//android.widget.EditText[@text='Amount']";
+    private static final String SUBMIT_BUTTON = "//android.widget.Button[@content-desc='DEPOSIT']";
+    private static final String ALERT_MESSAGE = "//android.widget.TextView[contains(@text, 'deposit')]";
+
+    @Step("Enter account ID: {accountId}")
+    public DepositPage enterAccountId(String accountId) {
+        WebElement accountIdElement = getWebDriver().findElement(MobileBy.xpath(ACCOUNT_ID_INPUT));
+        accountIdElement.clear();
+        accountIdElement.sendKeys(accountId);
+        return this;
+    }
+
+    @Step("Enter account number: {accountNumber}")
+    public DepositPage enterAccountNumber(String accountNumber) {
+        WebElement accountNumberElement = getWebDriver().findElement(MobileBy.xpath(ACCOUNT_NUMBER_INPUT));
+        accountNumberElement.clear();
+        accountNumberElement.sendKeys(accountNumber);
+        return this;
+    }
+
+    @Step("Enter amount: {amount}")
+    public DepositPage enterAmount(float amount) {
+        WebElement amountElement = getWebDriver().findElement(MobileBy.xpath(AMOUNT_INPUT));
+        amountElement.clear();
+        amountElement.sendKeys(String.valueOf(amount));
+        return this;
+    }
+
+    @Step("Submit deposit")
+    public DepositPage submitDeposit() {
+        $(MobileBy.xpath(SUBMIT_BUTTON)).should(Condition.visible).click();
+        return this;
+    }
+
+    @Step("Deposit {amount} to account id={accountId}, number={accountNumber}")
+    public DepositPage depositMoney(Long accountId, String accountNumber, float amount) {
+        log.debug("Deposit: accountId={}, accountNumber={}, amount={}", accountId, accountNumber, amount);
+        enterAccountId(accountId != null ? String.valueOf(accountId) : "");
+        enterAccountNumber(accountNumber);
+        enterAmount(amount);
+        submitDeposit();
+        return this;
+    }
+
+    @Step("Check alert shows success message and amount/account")
+    public DepositPage checkAlertMessageAndAmountAndAccountNumberAccept(String expectedMessage, float amount, String accountNumber) {
+        $(MobileBy.xpath(ALERT_MESSAGE)).should(Condition.visible);
+        return this;
+    }
+}
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,20 +4,22 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 import com.codeborne.selenide.Condition;
-
-import org.openqa.selenium.WebElement;
-
 import io.appium.java_client.MobileBy;
 import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginPage {
+    private static final Logger log = LoggerFactory.getLogger(LoginPage.class);
     private static final String LOGIN_TITLE = "//android.widget.TextView[@text='Login']";
     private static final String USERNAME_INPUT = "//android.widget.EditText[@text='Username']";
     private static final String PASSWORD_INPUT = "//android.widget.EditText[@text='Password']";
     private static final String LOGIN_BUTTON = "//*[@clickable='true'][.//android.widget.TextView[@text='Login']]";
 
     @Step("Insert username: {username}")
-    public LoginPage enterUsername(String username){
+    public LoginPage enterUsername(String username) {
+        log.debug("Entering username: {}", username);
         WebElement usernameElement = getWebDriver().findElement(MobileBy.xpath(USERNAME_INPUT));
         usernameElement.clear();
         usernameElement.sendKeys(username);
@@ -26,9 +28,10 @@ public class LoginPage {
 
     @Step("Insert password: {password}")
     public LoginPage enterPassword(String password) {
-        WebElement usernameElement = getWebDriver().findElement(MobileBy.xpath(PASSWORD_INPUT));
-        usernameElement.clear();
-        usernameElement.sendKeys(password);
+        log.debug("Entering password: {}", password);
+        WebElement passwordElement = getWebDriver().findElement(MobileBy.xpath(PASSWORD_INPUT));
+        passwordElement.clear();
+        passwordElement.sendKeys(password);
         return this;
     }
 
@@ -38,6 +41,7 @@ public class LoginPage {
     }
 
     public UserDashboardPage clickLoginButtonAndOpenUserDashboard() {
+        log.debug("Click Login -> User Dashboard");
         $(MobileBy.xpath(LOGIN_BUTTON)).should(Condition.visible).click();
         return new UserDashboardPage();
     }
