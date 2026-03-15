d99e4e92a171805202c18aceeecca47394be33d5
artem <artemminsadyrov@gmail.com>
2026-03-15 14:01:43 +0100

    Integrate backend logging and API health check in BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -16,8 +16,11 @@ import helpers.ApiHelper;
 import helpers.ScriptHelper;
 import io.qameta.allure.selenide.AllureSelenide;
 import models.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BaseTest {
+    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
     private static final String START_EMULATOR_SCRIPT_NAME = "start-emulator.sh";
     private static final String STOP_EMULATOR_SCRIPT_NAME = "stop-emulator.sh";
     private static final String ANDROID_HOME = "ANDROID_HOME";
@@ -26,6 +29,8 @@ public class BaseTest {
 
     @BeforeAll
     public static void setup() {
+        log.info("BaseTest setup: enable backend logging, health check, start emulator");
+        helpers.BackendLogging.install();
         ApiHelper.healthCheck();
 
         ScriptHelper.putEnvVariable(ANDROID_HOME, Config.getProperty("android.home"));
@@ -35,20 +40,24 @@ public class BaseTest {
         Configuration.browser = EmulatorDriver.class.getName();
         Configuration.browserSize=null;
         Configuration.timeout = 6000;
+        log.info("Selenide/Appium configured, timeout={} ms", Configuration.timeout);
     }
 
     @BeforeEach
     public void startDriver() {
+        log.debug("Opening driver (Selenide.open)");
         step("Open driver", () -> Selenide.open());
     }
 
     @AfterEach
     public void stopDriver() {
+        log.debug("Closing driver");
         step("Close driver", Selenide::closeWebDriver);
     }
 
     @AfterAll
     public static void clean() {
+        log.info("BaseTest clean: stopping emulator");
         ScriptHelper.execute(STOP_EMULATOR_SCRIPT_NAME);
     }
 }
